0f6df06435f36620fa5797a6a00e91d9ded37bbb
ALKONDR <kondrashkin.98@mail.ru>
2017-06-24 15:20:31 +0300

    second task implemented

--- app.js
+++ app.js
@@ -4,22 +4,40 @@ const t1Range = document.querySelector('.task1_range');
 const t1Mods = document.querySelector('.task1_mods');
 const t1Ans = document.querySelector('.task1_ans');
 
-console.dir(t1Ans);
-
 function calcTask1Ans(e) {
   let ans = 0;
 
   const range = t1Range.value.trim().split(' ').map(el => parseInt(el));
   const mods = t1Mods.value.trim().split(' ').map(el => parseInt(el));
 
-  for (let i = range[0]; i < range[1]; i++) {
+  for (let i = range[0]; i <= range[1]; i++) {
     ans += mods.every(el => i % el === 0);
   }
-
-  console.log(range, mods);
 
   t1Ans.textContent = ans;
 }
 
 t1Range.onkeyup = calcTask1Ans;
 t1Mods.onkeyup = calcTask1Ans;
+
+// task2
+
+const t2Word = document.querySelector('.task2_word');
+const t2Ans = document.querySelector('.task2_ans');
+
+const factorial = (val) => val <= 1 ? val : val * factorial(val - 1);
+
+function calcTask2Ans(e) {
+  const word = t2Word.value.split('');
+  const unique = word.filter((val, ind, self) => self.indexOf(val) === ind);
+
+  ans = factorial(word.length);
+
+  console.log(word, unique);
+
+  unique.forEach(letter => ans /= factorial(word.filter(el => el === letter).length));
+
+  t2Ans.textContent = ans;
+}
+
+t2Word.onkeyup = calcTask2Ans;
